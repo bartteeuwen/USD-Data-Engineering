e401d6ded3c2569494bd754c9ba13133237c1fe3
bteeuwen <bteeuwen@sandiego.edu>
2026-02-11 23:03:20 -0800

    Fix: point dim_fortune_500 to fortune_500_raw

--- transformation/01_dim_fortune_500.sql
+++ transformation/01_dim_fortune_500.sql
@@ -7,5 +7,5 @@ SELECT
   TRIM(state) AS state,
   TRIM(website) AS website,
   SAFE_CAST(employees AS INT64) AS employees
-FROM `usd-data-engineering.labor_market.fortune_500`
+FROM `usd-data-engineering.labor_market.fortune_500_raw`
 WHERE company IS NOT NULL;
