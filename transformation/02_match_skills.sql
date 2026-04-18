cd73523b8f9354c8af55a8a3649d0dc461461557
bartteeuwen <78899124+bartteeuwen@users.noreply.github.com>
2026-04-17 22:38:08 -0700

    Update 02_match_skills.sql

--- transformation/02_match_skills.sql
+++ transformation/02_match_skills.sql
@@ -1,19 +1,18 @@
 CREATE OR REPLACE TABLE `usd-data-engineering.labor_market.skills_in_demand` AS
 WITH unique_skills AS (
     SELECT DISTINCT technology_skill
-    FROM `usd-data-engineering.labor_market.O_Net_Technology_Skills`
+    FROM `usd-data-engineering.labor_market.O_Net_Technology_Skills_normalized`
     WHERE LENGTH(technology_skill) > 2
 )
 SELECT DISTINCT
     j.id AS job_id,
     j.title,
     j.company,
-    DATETIME(PARSE_TIMESTAMP('%Y-%m-%d %H:%M:%S', j.ingested_at), 'America/Los_Angeles') as ingested_at,
+    DATETIME(PARSE_TIMESTAMP('%Y-%m-%d %H:%M:%S', j.ingested_at), 'America/Los_Angeles') AS ingested_at,
     s.technology_skill AS skill_name
 FROM `usd-data-engineering.labor_market.raw_job_postings` j
 CROSS JOIN unique_skills s
 WHERE STRPOS(LOWER(j.description), LOWER(s.technology_skill)) > 0;
 
--- Validation
 ASSERT (SELECT COUNT(*) FROM `usd-data-engineering.labor_market.skills_in_demand`) > 0
   AS 'Error: No skills were matched. Check cross-join logic in skills_in_demand.';
