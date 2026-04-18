339fef30e2789eda741d39108962cc763471adbc
bartteeuwen <78899124+bartteeuwen@users.noreply.github.com>
2026-04-17 22:43:11 -0700

    Update 01_dim_fortune_500.sql

--- transformation/01_dim_fortune_500.sql
+++ transformation/01_dim_fortune_500.sql
@@ -6,7 +6,7 @@ SELECT DISTINCT
   TRIM(city) AS city,
   TRIM(state) AS state,
   website
-FROM `usd-data-engineering.labor_market.fortune_500_raw`
+FROM `usd-data-engineering.labor_market.dim_fortune_500`
 WHERE company IS NOT NULL;
 
 ASSERT (SELECT COUNT(*) FROM `usd-data-engineering.labor_market.dim_fortune_500`) > 0
